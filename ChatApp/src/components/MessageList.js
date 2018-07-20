26d0e81f343fa771b8b6473db89eebef1a43751d
Xiaotian Chen <sargrass@gmail.com>
2018-07-19 21:13:07 -0700

    fix undefined user

--- ChatApp/src/components/MessageList.js
+++ ChatApp/src/components/MessageList.js
@@ -9,10 +9,10 @@ import moment from 'moment';
 const Message = ({ msg }) => (
     <Row>
         <Image styleName="small-avatar top"
-               source={{ uri: msg.author.avatar }} />
+               source={{ uri: msg.author ? msg.author.avatar : 'https://abs.twimg.com/sticky/default_profile_images/default_profile_3_400x400.png'}} />
         <View styleName="vertical">
             <View styleName="horizontal space-between">
-                <Subtitle>{msg.author.name}</Subtitle>
+                <Subtitle>{msg.author ? msg.author.name : 'Unknown'}</Subtitle>
                 <Caption>{moment(msg.time).from(Date.now())}</Caption>
             </View>
             <Text styleName="multiline">{msg.text}</Text>
