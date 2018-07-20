b799b2d87c26ad704a7c2929314c7c7a4428052a
Xiaotian Chen <sargrass@gmail.com>
2018-07-19 21:15:36 -0700

    note .sh for install, probably not neccessary

---
+++ ChatApp/install.sh
@@ -0,0 +1,12 @@
+set -ex
+
+react-native install firebase
+react-native install moment
+react-native install redux
+react-native install react-redux
+react-native install redux-thunk
+react-native install redux-logger
+react-native install react-native-keyboard-aware-scroll-view
+react-native install react-native-device-info
+react-native install @shoutem/ui
+
