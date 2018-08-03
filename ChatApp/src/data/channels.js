fe66b20d4c457b73aea4d1584590459d2b885e8b
Xiaotian Chen <sargrass@gmail.com>
2018-08-03 12:21:52 -0700

    new channels

--- ChatApp/src/data/channels.js
+++ ChatApp/src/data/channels.js
@@ -1,24 +1,108 @@
 const channels = {
+	'group_id_0': {
+		'精选': [
+			{
+				id: 'channel_id_1',
+				title: '热门公号'
+			},
+			{
+				id: 'channel_id_2',
+				title: '为你精选'
+			},
+		],
+		'分类排行': [
+			{
+				id: 'channel_id_4',
+				title: '游戏'
+			},
+			{
+				id: 'channel_id_5',
+				title: '明星'
+			},
+			{
+				id: 'channel_id_6',
+				title: '动漫'
+			},
+			{
+				id: 'channel_id_7',
+				title: '主播'
+			},
+			{
+				id: 'channel_id_12',
+				title: '影视'
+			},
+			{
+				id: 'channel_id_3',
+				title: '综艺'
+			},
+			{
+				id: 'channel_id_13',
+				title: '音乐'
+			},
+			{
+				id: 'channel_id_13',
+				title: '搞笑'
+			},
+			{
+				id: 'channel_id_13',
+				title: '科技'
+			},
+			{
+				id: 'channel_id_13',
+				title: '文学'
+			},
+			{
+				id: 'channel_id_13',
+				title: '投资'
+			},
+			{
+				id: 'channel_id_13',
+				title: '学习'
+			},
+			{
+				id: 'channel_id_13',
+				title: '旅游'
+			},
+		],
+		'地区排行': [
+			{
+				id: 'channel_id_8',
+				title: '北京'
+			},
+			{
+				id: 'channel_id_9',
+				title: '深圳'
+			},
+		],
+	},
 	'group_id_1': {
 		'置顶频道': [
 			{
 				id: 'channel_id_1',
-				title: '入群须知'
-			},
-			{
-				id: 'channel_id_11',
-				title: '官方文档'
+				title: '规则'
 			},
 			{
 				id: 'channel_id_2',
 				title: '公告'
 			},
 			{
+				id: 'channel_id_11',
+				title: '官方文档'
+			},
+			{
 				id: 'channel_id_10',
 				title: '工具推荐'
 			},
+			{
+				id: 'channel_id_10',
+				title: '私信管理员'
+			},
 		],
 		'聊天频道': [
+			{
+				id: 'channel_id_13',
+				title: '大厅'
+			},
 			{
 				id: 'channel_id_4',
 				title: '问答-Tensorflow'
@@ -43,10 +127,6 @@ const channels = {
 				id: 'channel_id_3',
 				title: '新闻讨论'
 			},
-			{
-				id: 'channel_id_13',
-				title: '闲聊'
-			},
 		],
 		'城市频道': [
 			{
@@ -58,38 +138,54 @@ const channels = {
 				title: '深圳'
 			},
 		],
+		'私聊': [
+			{
+				id: 'channel_id_23',
+				title: 'Rick'
+			},
+			{
+				id: 'channel_id_23',
+				title: 'Morty'
+			},
+		],
+		'链接': [
+			{
+				id: 'channel_id_23',
+				title: '互联网变革的这十年：2008-2018'
+			},
+		],
 	},
 	'group_id_2': {
 		'置顶频道': [
 			{
 				id: 'channel_id_14',
-				title: '入群须知'
+				title: '规则'
 			},
 			{
 				id: 'channel_id_15',
-				title: '游戏公告'
+				title: '公告'
 			},
 			{
 				id: 'channel_id_10',
 				title: '道具商城'
 			},
+			{
+				id: 'channel_id_15',
+				title: '官网链接'
+			},
+			{
+				id: 'channel_id_10',
+				title: '私信管理员'
+			},
 		],
-		'聊天频道': [
+		'帖子': [
 			{
-				id: 'channel_id_16',
-				title: '闲聊'
+				id: 'channel_id_19',
+				title: '炉石相关'
 			},
 			{
-				id: 'channel_id_20',
-				title: '约战'
-			},
-			{
-				id: 'channel_id_20',
-				title: '新卡讨论'
-			},
-			{
-				id: 'channel_id_18',
-				title: '锦标赛: 7/30-8/5'
+				id: 'channel_id_17',
+				title: '其它游戏'
 			},
 		],
 		'直播频道': [
@@ -102,23 +198,125 @@ const channels = {
 				title: '哀绿: 8/4 13:00'
 			},
 		],
+		'聊天频道': [
+			{
+				id: 'channel_id_16',
+				title: '大厅'
+			},
+			{
+				id: 'channel_id_20',
+				title: '约战'
+			},
+			{
+				id: 'channel_id_20',
+				title: '新卡讨论'
+			},
+			{
+				id: 'channel_id_18',
+				title: '天梯讨论'
+			},
+			{
+				id: 'channel_id_18',
+				title: '竞技场讨论'
+			},
+			{
+				id: 'channel_id_20',
+				title: '乱斗讨论'
+			},
+			{
+				id: 'channel_id_18',
+				title: '锦标赛: 7/30-8/5'
+			},
+			{
+				id: 'channel_id_18',
+				title: '职业讨论: 战士'
+			},
+			{
+				id: 'channel_id_18',
+				title: '职业讨论: 法师'
+			},
+		],
+		'私聊': [
+			{
+				id: 'channel_id_23',
+				title: 'Beth'
+			},
+			{
+				id: 'channel_id_23',
+				title: 'Jerry'
+			},
+		],
+		'链接': [
+			{
+				id: 'channel_id_23',
+				title: '一千万奖金！暴雪官方炉石比赛再破纪录'
+			},
+		],
 	},
 	'group_id_3': {
 		'置顶频道': [
 			{
 				id: 'channel_id_21',
-				title: '入群须知'
+				title: '规则'
 			},
 			{
 				id: 'channel_id_22',
 				title: '公告'
 			},
-		],
-		'聊天频道': [
 			{
-				id: 'channel_id_23',
-				title: '闲聊'
+				id: 'channel_id_10',
+				title: '私信管理员'
 			},
+		],
+		'固定讨论': [
+			{
+				id: 'channel_id_25',
+				title: '凡凡动态'
+			},
+			{
+				id: 'channel_id_25',
+				title: '潮流讨论'
+			},
+			{
+				id: 'channel_id_25',
+				title: '说唱讨论'
+			},
+			{
+				id: 'channel_id_25',
+				title: '电影讨论'
+			},
+		],
+		'事件讨论': [
+			{
+				id: 'channel_id_25',
+				title: '虎扑事件'
+			},
+			{
+				id: 'channel_id_25',
+				title: '九寨沟地震'
+			},
+		],
+		'线下活动': [
+			{
+				id: 'channel_id_25',
+				title: '8/30 上海演唱会'
+			},
+			{
+				id: 'channel_id_25',
+				title: '9/10 中国新说唱第5期录制现场'
+			},
+		],
+		'活动筹备': [
+			{
+				id: 'channel_id_24',
+				title: '9/2北京接机'
+			},
+			{
+				id: 'channel_id_24',
+				title: '11/6生日'
+			},
+		],
+		'职能组': [
 			{
 				id: 'channel_id_25',
 				title: '图片制作组'
@@ -131,21 +329,21 @@ const channels = {
 				id: 'channel_id_25',
 				title: '贴吧组'
 			},
+		],
+		'私聊': [
 			{
-				id: 'channel_id_25',
-				title: '虎扑事件'
+				id: 'channel_id_23',
+				title: 'Jerry'
 			},
 			{
-				id: 'channel_id_25',
-				title: '九寨沟地震捐款'
+				id: 'channel_id_23',
+				title: 'Summer'
 			},
+		],
+		'链接': [
 			{
-				id: 'channel_id_24',
-				title: '8/30上海演唱会'
-			},
-			{
-				id: 'channel_id_24',
-				title: '9/2北京接机'
+				id: 'channel_id_23',
+				title: '新歌发布！吴亦凡再度证明实力！'
 			},
 		],
 	},
@@ -153,7 +351,77 @@ const channels = {
 		'置顶频道': [
 			{
 				id: 'channel_id_21',
-				title: '入群须知'
+				title: '规则'
+			},
+			{
+				id: 'channel_id_22',
+				title: '公告'
+			},
+			{
+				id: 'channel_id_10',
+				title: '私信管理员'
+			},
+			{
+				id: 'channel_id_19',
+				title: '节目组官方: 8/4 17:30'
+			},
+		],
+		'赞助商商城': [
+			{
+				id: 'channel_id_17',
+				title: 'OPPO'
+			},
+			{
+				id: 'channel_id_17',
+				title: '伊利'
+			},
+		],
+		'聊天频道': [
+			{
+				id: 'channel_id_23',
+				title: '大厅'
+			},
+			{
+				id: 'channel_id_25',
+				title: '第7期讨论'
+			},
+			{
+				id: 'channel_id_25',
+				title: '第6期讨论'
+			},
+		],
+		'粉丝频道': [
+			{
+				id: 'channel_id_23',
+				title: '王菊粉丝频道'
+			},
+			{
+				id: 'channel_id_25',
+				title: '杨超越粉丝频道'
+			},
+		],
+		'私聊': [
+			{
+				id: 'channel_id_23',
+				title: 'Beth'
+			},
+			{
+				id: 'channel_id_23',
+				title: 'Morty'
+			},
+		],
+		'链接': [
+			{
+				id: 'channel_id_23',
+				title: '101唱功排名，你的小姐姐上榜了吗？'
+			},
+		],
+	},
+	'group_id_5': {
+		'置顶频道': [
+			{
+				id: 'channel_id_21',
+				title: '规则'
 			},
 			{
 				id: 'channel_id_22',
@@ -161,10 +429,28 @@ const channels = {
 			},
 			{
 				id: 'channel_id_22',
-				title: '金融产品推荐'
+				title: '理财产品推荐'
+			},
+			{
+				id: 'channel_id_10',
+				title: '私信管理员'
+			},
+		],
+		'付费频道': [
+			{
+				id: 'channel_id_25',
+				title: 'Rick的咨询频道'
+			},
+			{
+				id: 'channel_id_25',
+				title: 'Morty的咨询频道'
 			},
 		],
 		'聊天频道': [
+			{
+				id: 'channel_id_13',
+				title: '大厅'
+			},
 			{
 				id: 'channel_id_24',
 				title: 'A股讨论'
@@ -182,24 +468,342 @@ const channels = {
 				title: '房市讨论'
 			},
 			{
-				id: 'channel_id_25',
-				title: '期货讨论'
+				id: 'channel_id_22',
+				title: '基金讨论'
 			},
 			{
 				id: 'channel_id_25',
 				title: '加密货币讨论'
 			},
 		],
-		'付费频道': [
+		'私聊': [
 			{
-				id: 'channel_id_25',
-				title: 'AAA的咨询频道'
+				id: 'channel_id_23',
+				title: 'Rick'
+			},
+			{
+				id: 'channel_id_23',
+				title: 'Summer'
+			},
+		],
+		'链接': [
+			{
+				id: 'channel_id_23',
+				title: '人民币汇率：短期承压破7，中长期回升新高'
+			},
+		],
+	},
+	'group_id_6': {
+		'置顶频道': [
+			{
+				id: 'channel_id_21',
+				title: '规则'
+			},
+			{
+				id: 'channel_id_22',
+				title: '公告'
+			},
+			{
+				id: 'channel_id_10',
+				title: '提问keso'
+			},
+			{
+				id: 'channel_id_19',
+				title: '招聘'
+			},
+			{
+				id: 'channel_id_19',
+				title: '文章列表'
+			},
+			{
+				id: 'channel_id_19',
+				title: '视频内容'
+			},
+			{
+				id: 'channel_id_19',
+				title: 'keso小铺'
+			},
+			{
+				id: 'channel_id_19',
+				title: '赞助商商城'
+			},
+		],
+		'聊天频道': [
+			{
+				id: 'channel_id_23',
+				title: '大厅'
 			},
 			{
 				id: 'channel_id_25',
-				title: 'BBB的咨询频道'
+				title: '话题建议'
 			},
-		]
+		],
+		'私聊': [
+			{
+				id: 'channel_id_23',
+				title: 'Jerry'
+			},
+			{
+				id: 'channel_id_23',
+				title: 'Beth'
+			},
+		],
+		'链接': [
+			{
+				id: 'channel_id_23',
+				title: '百度的第二条命'
+			},
+		],
+	},
+	'group_id_7': {
+		'置顶频道': [
+			{
+				id: 'channel_id_21',
+				title: '欢迎信息'
+			},
+			{
+				id: 'channel_id_22',
+				title: '公告'
+			},
+			{
+				id: 'channel_id_10',
+				title: '私信管理员'
+			},
+			{
+				id: 'channel_id_19',
+				title: '招聘'
+			},
+			{
+				id: 'channel_id_19',
+				title: '加盟'
+			},
+		],
+		'公开频道': [
+			{
+				id: 'channel_id_23',
+				title: '大厅'
+			},
+		],
+		'会员频道': [
+			{
+				id: 'channel_id_23',
+				title: '大学申请讨论'
+			},
+			{
+				id: 'channel_id_23',
+				title: 'SAT讨论'
+			},
+			{
+				id: 'channel_id_23',
+				title: '高中学业讨论'
+			},
+			{
+				id: 'channel_id_23',
+				title: '家长交流'
+			},
+		],
+		'高级会员频道': [
+			{
+				id: 'channel_id_23',
+				title: '全方位导师'
+			},
+		],
+		'私聊': [
+			{
+				id: 'channel_id_23',
+				title: 'Beth'
+			},
+			{
+				id: 'channel_id_23',
+				title: 'Jerry'
+			},
+		],
+		'链接': [
+			{
+				id: 'channel_id_23',
+				title: '背单词的5个诀窍'
+			},
+		],
+	},
+	'group_id_8': {
+		'置顶频道': [
+			{
+				id: 'channel_id_21',
+				title: '规则'
+			},
+			{
+				id: 'channel_id_22',
+				title: '公告'
+			},
+			{
+				id: 'channel_id_10',
+				title: '私信管理员'
+			},
+			{
+				id: 'channel_id_19',
+				title: '招聘'
+			},
+			{
+				id: 'channel_id_19',
+				title: '文章列表'
+			},
+			{
+				id: 'channel_id_19',
+				title: '视频内容'
+			},
+		],
+		'公共频道': [
+			{
+				id: 'channel_id_23',
+				title: '大厅'
+			},
+		],
+		'咨询频道': [
+			{
+				id: 'channel_id_23',
+				title: 'Rick: 8/25 19:30'
+			},
+		],
+		'私聊': [
+			{
+				id: 'channel_id_23',
+				title: 'Summer'
+			},
+			{
+				id: 'channel_id_23',
+				title: 'Beth'
+			},
+		],
+		'链接': [
+			{
+				id: 'channel_id_23',
+				title: '如何快速减压'
+			},
+		],
+	},
+	'group_id_9': {
+		'置顶频道': [
+			{
+				id: 'channel_id_21',
+				title: '订阅须知'
+			},
+			{
+				id: 'channel_id_22',
+				title: '公告'
+			},
+			{
+				id: 'channel_id_10',
+				title: '私信管理员'
+			},
+		],
+		'聊天频道': [
+			{
+				id: 'channel_id_23',
+				title: '大厅'
+			},
+			{
+				id: 'channel_id_23',
+				title: '招人和跳槽'
+			},
+			{
+				id: 'channel_id_23',
+				title: '技术交流'
+			},
+			{
+				id: 'channel_id_23',
+				title: '职场交流'
+			},
+			{
+				id: 'channel_id_23',
+				title: '搞笑转帖'
+			},
+			{
+				id: 'channel_id_23',
+				title: '约游戏开黑'
+			},
+			{
+				id: 'channel_id_23',
+				title: '约线下活动'
+			},
+			{
+				id: 'channel_id_23',
+				title: '约其他'
+			},
+		],
+		'私聊': [
+			{
+				id: 'channel_id_23',
+				title: 'Rick'
+			},
+			{
+				id: 'channel_id_23',
+				title: 'Morty'
+			},
+		],
+		'链接': [
+			{
+				id: 'channel_id_23',
+				title: '哈哈哈哈哈笑死我了！'
+			},
+		],
+	},
+	'group_id_10': {
+		'置顶频道': [
+			{
+				id: 'channel_id_21',
+				title: '欢迎信息'
+			},
+			{
+				id: 'channel_id_22',
+				title: '公告'
+			},
+			{
+				id: 'channel_id_10',
+				title: '私信管理员'
+			},
+			{
+				id: 'channel_id_19',
+				title: '商店'
+			},
+			{
+				id: 'channel_id_19',
+				title: '9月新款'
+			},
+		],
+		'公开频道': [
+			{
+				id: 'channel_id_23',
+				title: '大厅'
+			},
+			{
+				id: 'channel_id_23',
+				title: '新款征集'
+			},
+			{
+				id: 'channel_id_23',
+				title: '补款'
+			},
+			{
+				id: 'channel_id_23',
+				title: '二手交易'
+			},
+		],
+		'私聊': [
+			{
+				id: 'channel_id_23',
+				title: 'Morty'
+			},
+			{
+				id: 'channel_id_23',
+				title: 'Rick'
+			},
+		],
+		'链接': [
+			{
+				id: 'channel_id_23',
+				title: '11位instagram上最萌插画师'
+			},
+		],
 	},
 };
 
--- ChatApp/src/data/groups.js
+++ ChatApp/src/data/groups.js
@@ -1,23 +1,58 @@
 const groups = [
 	{
+		id: 'group_id_0',
+		name: '门户',
+		icon: require('../assets/icons/group_icons/portal@256.png')
+	},
+	{
 		id: 'group_id_1',
 		name: 'Tensorflow',
 		icon: require('../assets/icons/group_icons/tensorflow@256.png')
 	},
 	{
 		id: 'group_id_2',
-		name: 'Hearthstone',
+		name: '炉石传说',
 		icon: require('../assets/icons/group_icons/hearthstone@256.png')
 	},
 	{
 		id: 'group_id_3',
-		name: 'Wuyifan',
+		name: '吴亦凡',
 		icon: require('../assets/icons/group_icons/wuyifan@256.png')
 	},
 	{
 		id: 'group_id_4',
-		name: 'Stock',
+		name: '创造101',
+		icon: require('../assets/icons/group_icons/101@256.jpg')
+	},
+	{
+		id: 'group_id_5',
+		name: '股市投资',
 		icon: require('../assets/icons/group_icons/stock@256.png')
+	},
+	{
+		id: 'group_id_6',
+		name: 'keso怎么看',
+		icon: require('../assets/icons/group_icons/keso@300.jpeg')
+	},
+	{
+		id: 'group_id_7',
+		name: 'ivyGoal',
+		icon: require('../assets/icons/group_icons/ivy@247.png')
+	},
+	{
+		id: 'group_id_8',
+		name: 'CC咨询',
+		icon: require('../assets/icons/group_icons/mindful@256.png')
+	},
+	{
+		id: 'group_id_9',
+		name: '望京码农',
+		icon: require('../assets/icons/group_icons/coder@256.jpg')
+	},
+	{
+		id: 'group_id_10',
+		name: '裙摆',
+		icon: require('../assets/icons/group_icons/lolita@180.jpeg')
 	},
 ];
 
